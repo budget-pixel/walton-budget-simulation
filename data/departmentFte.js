5cd9fc1b0376141c11869cc8c543bbdbeb3cbcf7
budget-pixel <peydevon@gmail.com>
2026-06-07 16:20:22 -0500

    Update departmentFte.js

--- data/departmentFte.js
+++ data/departmentFte.js
@@ -1,5 +1,114 @@
 window.departmentFte = {
-  version: 1,
-  generatedAt: "1970-01-01T00:00:00.000Z",
-  departments: {}
+  "version": 1,
+  "generatedAt": "2026-06-07T21:17:59.711Z",
+  "departments": {
+    "sheriff-s-office": {
+      "department": "Sheriff's Office",
+      "fte": 669
+    },
+    "board-of-county-commissioners": {
+      "department": "Board of County Commissioners",
+      "fte": 10.5
+    },
+    "building-construction-and-maintenance": {
+      "department": "Building Construction and Maintenance",
+      "fte": 68
+    },
+    "tax-collector": {
+      "department": "Tax Collector",
+      "fte": 40
+    },
+    "planning": {
+      "department": "Planning",
+      "fte": 47
+    },
+    "clerk-of-court": {
+      "department": "Clerk of Court",
+      "fte": 80
+    },
+    "property-appraiser": {
+      "department": "Property Appraiser",
+      "fte": 38
+    },
+    "code-compliance": {
+      "department": "Code Compliance",
+      "fte": 43
+    },
+    "engineering-services": {
+      "department": "Engineering Services",
+      "fte": 15
+    },
+    "county-administration": {
+      "department": "County Administration",
+      "fte": 16
+    },
+    "libraries": {
+      "department": "Libraries",
+      "fte": 22.5
+    },
+    "eagle-springs-golf-and-recreation-center": {
+      "department": "Eagle Springs Golf and Recreation Center",
+      "fte": 12.5
+    },
+    "office-of-the-county-attorney": {
+      "department": "Office of the County Attorney",
+      "fte": 10
+    },
+    "supervisor-of-elections": {
+      "department": "Supervisor of Elections",
+      "fte": 10
+    },
+    "human-resources": {
+      "department": "Human Resources",
+      "fte": 13
+    },
+    "office-of-management-and-budget": {
+      "department": "Office of Management and Budget",
+      "fte": 7
+    },
+    "procurement": {
+      "department": "Procurement",
+      "fte": 10
+    },
+    "recreation": {
+      "department": "Recreation",
+      "fte": 6
+    },
+    "south-walton-fire-district": {
+      "department": "South Walton Fire District",
+      "fte": 0
+    },
+    "emergency-management": {
+      "department": "Emergency Management",
+      "fte": 6
+    },
+    "environmental-services": {
+      "department": "Environmental Services",
+      "fte": 4
+    },
+    "geographic-info-systems": {
+      "department": "Geographic Info Systems",
+      "fte": 6
+    },
+    "extension-office": {
+      "department": "Extension Office",
+      "fte": 8.5
+    },
+    "eagle-springs-grill": {
+      "department": "Eagle Springs Grill",
+      "fte": 6
+    },
+    "probation-services": {
+      "department": "Probation Services",
+      "fte": 4
+    },
+    "veteran-services": {
+      "department": "Veteran Services",
+      "fte": 3
+    },
+    "soil-conservation": {
+      "department": "Soil Conservation",
+      "fte": 2
+    }
+  }
 };
